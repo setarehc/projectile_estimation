9f4a0a0850b59ea9a11c1941abe68db3371dd388
setarehcohan <setareh.cohan@gmail.com>
2018-05-28 14:20:33 -0700

    objective_function.m updated.

--- objective_function.m
+++ objective_function.m
@@ -1,6 +1,6 @@
-function [ J ] = objective_function( var, X, hypothesis_function)
-%definition of objective function in terms of variables(var=[v0x; v0y; Fx; Fy])
-    %, X=training data [t;x;y] and hypothesis function
+function [ J ] = objective_function(var, X, hypothesis_function)
+% Definition of the objective function in terms of variables(var=[v0x; v0y;
+    % Fx; Fy]), X=training data [t;x;y] and the hypothesis function.
     temp = 0;
     m = size(X,1);
     for i=1:m
