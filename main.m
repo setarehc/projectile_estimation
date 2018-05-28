8bf2bd4d6cad6059c113dd3e18a40d03e5717c86
setarehcohan <setareh.cohan@gmail.com>
2018-05-28 14:14:33 -0700

    main.m updated.

--- main.m
+++ main.m
@@ -1,3 +1,3 @@
-% The main file which runs the whole program.
+% The main file which runs the code.
 trajectory = predict_xy( 10, pi/4, [1;1;1;1], 15, 0.012,30);
 plot(trajectory(:,2),trajectory(:,3));
